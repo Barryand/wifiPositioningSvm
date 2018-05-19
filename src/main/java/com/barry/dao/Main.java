fc673e5ddbd7f44fac4d60923b7d1611f8aa835f
Barry <295671947@qq.com>
2018-05-19 20:34:51 +0900

    130

--- src/main/java/com/barry/dao/Main.java
+++ src/main/java/com/barry/dao/Main.java
@@ -35,17 +35,17 @@ public class Main {
         String[] sarg = {"-l", "0", "-u", "1", "-s", "trainfile\\scalerule.txt", //存放SVM缩放规则的路径
                 "-z", recordFilename//存放缩放后数据的路径,这里是覆盖掉原始文件
                 , recordFilename};//需要缩放的数据
-        String[] targ = { "-c", "8", "-g", "8", //c和gamma是事先用grid.py计算的结果
+        String[] targ = { "-c", "128", "-g", "0.5", //c和gamma是事先用grid.py计算的结果
                 recordFilename, //存放SVM训练模型用的数据的路径
                 "trainfile\\model_r.txt"}; //存放SVM通过训练数据训练出来的模型的路径
         main.svmTraining(sarg, targ);
 
 //测试用,使用Main.main（...）时，将main的参数改为(Map<String, Double> userData, List<PointLocEntity> pointLocEntityList)
         Map<String, Double> userData = new HashMap<String, Double>();
-        String[] AP = {"TP-LINK_3051", "TP-LINK_35EB", "TP-LINK_3625", "TP-LINK_5958",
-                "TP-LINK_E7D2", "Four-Faith-2", "Four-Faith-3"};
-        double[] RSSI = {-47, -38, -45, -40, -46, -41, -41};
-        for (int i = 0; i < 7; i++) {
+        String[] AP = {"abc2", "abc3", "abc4", "abc6", "abc7", "abc8", "MERCURY_B932", "MERCURY_BD09",
+                "MERCURY_CFF9", "Xiaomi_31CB_CE34", "Xiaomi_3525_CADA"};
+        double[] RSSI = {-71, -57, -46, -33, -40, -39, -63, -0, -0, -65, -46};
+        for (int i = 0; i < 11; i++) {
             userData.put(AP[i], RSSI[i]);
         }
 
@@ -68,11 +68,10 @@ public class Main {
 
 
         String formatedUserData = null;
-        formatedUserData = main.userDataFormating(userData);//对用户上传的数据进行重新组织*/
+        formatedUserData = main.userDataFormating(userData);//对用户上传的数据进行重新组织
         String[] sarg2 = {"-r", "trainfile\\scalerule.txt"};//重载SVM缩放规则
         String[] parg = {"trainfile\\model_r.txt" }; //调用的是训练以后的模型
         main.svmPredicting(formatedUserData, pointLocEntityList, sarg2, parg);
-
     }
     /**
      *@描述   对训练文件进行重新组织
@@ -126,13 +125,21 @@ public class Main {
         }
     }
     private String reorganize(String tempLine, int MinuteHand) {
-        String[] AP = {"TP-LINK_3051", "TP-LINK_35EB", "TP-LINK_3625", "TP-LINK_5958",
-                "TP-LINK_E7D2", "Four-Faith-2", "Four-Faith-3"};
+        String[] AP = {"abc2", "abc3", "abc4", "abc6", "abc7", "abc8", "MERCURY_B932", "MERCURY_BD09",
+        "MERCURY_CFF9", "Xiaomi_31CB_CE34", "Xiaomi_3525_CADA"};
         StringBuilder sb = new StringBuilder(MinuteHand + "");
         Matcher matcher = Pattern.compile("\\d+").matcher(tempLine);
-        for (int i = 0; i < 7; i++) {
+        int offset = 0;   //为文件格式作补偿
+        for (int i = 0; i < 11; i++) {
+            if (i <= 5) {
+                offset = 6;
+            } else if (i <= 8) {
+                offset = 14;
+            } else {
+                offset = 18;
+            }
             if (tempLine.indexOf(AP[i]) != -1) { //当一行数据中有某个AP
-                matcher.find(tempLine.indexOf(AP[i]) + 14);
+                matcher.find(tempLine.indexOf(AP[i]) + offset);
                 sb.append(" ").append(i+1).append(":").append(matcher.group());
             } else {
                 sb.append(" ").append(i+1).append(":").append(0);
@@ -142,14 +149,14 @@ public class Main {
     }
 
     private String userDataFormating(Map<String, Double> userData) {
-        String[] AP = {"TP-LINK_3051", "TP-LINK_35EB", "TP-LINK_3625", "TP-LINK_5958",
-                "TP-LINK_E7D2", "Four-Faith-2", "Four-Faith-3"};
+        String[] AP = {"abc2", "abc3", "abc4", "abc6", "abc7", "abc8", "MERCURY_B932", "MERCURY_BD09",
+                "MERCURY_CFF9", "Xiaomi_31CB_CE34", "Xiaomi_3525_CADA"};
         List<Double> value = new ArrayList<Double>();
         for (String key : AP) {
             value.add(-userData.get(key));
         }
         String formatedUserData = 0 + " ";
-        for (int i = 1; i < 8; i++) {
+        for (int i = 1; i < 12; i++) {
             formatedUserData = formatedUserData + i + ":" + value.get(i-1) + " ";
         }
         return formatedUserData;
